bf7f6fc4959e307a9a2ad5aa796d1e0205a5b1e7
Joshua Watt <Joshua.Watt@garmin.com>
2019-02-01 12:40:09 -0600

    Coerce home paths when binding in docker
    If the user is trying to bind mount something from their home directory,
    it probably should be bind mounted onto the home directory of the user
    running in the pyrex container.

--- pyrex.py
+++ pyrex.py
@@ -30,6 +30,7 @@ import shlex
 import glob
 import textwrap
 import stat
+import pathlib
 
 THIS_SCRIPT = os.path.basename(__file__)
 PYREX_VERSION = '1'
@@ -312,7 +313,10 @@ def main():
 
             # Configure binds
             for b in config['docker']['bind'].split():
-                docker_args.extend(['--mount', 'type=bind,src={b},dst={b}'.format(b=b)])
+                src = b.replace('@HOME@', os.path.expanduser('~'))
+                dst = b.replace('@HOME@', config['pyrex']['home'])
+
+                docker_args.extend(['--mount', 'type=bind,src={src},dst={dst}'.format(src=src, dst=dst)])
 
             # Pass environment variables
             for e in config['docker']['envvars'].split():
