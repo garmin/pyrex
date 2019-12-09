69eb6e51b93a3a7227c5a91139737bc241327975
Joshua Watt <Joshua.Watt@garmin.com>
2019-12-09 13:40:25 -0600

    Bump version for release
    Bumps version for the 3rd beta release

--- pyrex.py
+++ pyrex.py
@@ -32,7 +32,7 @@ import json
 import tempfile
 import contextlib
 
-VERSION = "1.0.0-beta2"
+VERSION = "1.0.0-beta3"
 
 VERSION_REGEX = re.compile(r"^([0-9]+\.){2}[0-9]+(-.*)?$")
 VERSION_TAG_REGEX = re.compile(r"^v([0-9]+\.){2}[0-9]+(-.*)?$")
