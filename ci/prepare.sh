0489fee2a9e96996e874ed2a4a3625cc465ef281
Joshua Watt <Joshua.Watt@garmin.com>
2022-09-26 12:45:45 -0500

    Add Yocto 4.0 compatibility
    Adds compatibility with Yocto 4.0; specifically BB_ENV_WHITELIST was
    renamed BB_ENV_PASSTHROUGH_ADDITIONS
    
    Closes: #80

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -26,9 +26,14 @@ echo 'ef3d4305054282938bfe70dc5a08eba8a701a22b49795b1c2d8ed5aed90d0581 *poky/pok
 wget --no-check-certificate -O "$TOP_DIR/poky/poky-3.1.tar.bz2" "http://downloads.yoctoproject.org/releases/yocto/yocto-3.1/poky-dunfell-23.0.0.tar.bz2"
 echo 'c1f4a486e5f090dbdf50c15a5d22afa6689bd609604b48d63eb0643ad58eb370 *poky/poky-3.1.tar.bz2' | sha256sum -c
 
-mkdir "$TOP_DIR/poky/2.6" "$TOP_DIR/poky/3.1"
+wget --no-check-certificate -O "$TOP_DIR/poky/poky-4.0.tar.bz2" http://downloads.yoctoproject.org/releases/yocto/yocto-4.0/poky-00cfdde791a0176c134f31e5a09eff725e75b905.tar.bz2
+echo '4cedb491b7bf0d015768c61690f30d7d73f4266252d6fba907bba97eac83648c *poky/poky-4.0.tar.bz2' | sha256sum -c
+
+mkdir "$TOP_DIR/poky/2.6" "$TOP_DIR/poky/3.1" "$TOP_DIR/poky/4.0"
 echo "Extracting..."
 tar -xf "$TOP_DIR/poky/poky-2.6.tar.bz2" -C "$TOP_DIR/poky/2.6" --strip-components=1
 tar -xf "$TOP_DIR/poky/poky-3.1.tar.bz2" -C "$TOP_DIR/poky/3.1" --strip-components=1
+tar -xf "$TOP_DIR/poky/poky-4.0.tar.bz2" -C "$TOP_DIR/poky/4.0" --strip-components=1
 ln -s ../../pyrex-init-build-env "$TOP_DIR/poky/2.6/"
 ln -s ../../pyrex-init-build-env "$TOP_DIR/poky/3.1/"
+ln -s ../../pyrex-init-build-env "$TOP_DIR/poky/4.0/"
